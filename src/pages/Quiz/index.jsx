7bcb2fbcc5bbb5d1bfdbc855154e1ef656aa948e
madCakes <jason.pbrooks@hotmail.com>
2022-09-05 23:25:15 +0100

    add context

--- src/pages/Quiz/index.jsx
+++ src/pages/Quiz/index.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import {
+  Question,
+  Subject,
+  AnswerForm,
+  QuestionNumber,
+} from "../../components";
+import DataContext from "../../context/dataContext";
 
 const Quiz = () => {
-  return <div>Quiz</div>;
+  const { questions } = useContext(DataContext);
+  //Fetching quiz data:
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    try {
+      const response = await axios.get(
+        `https://opentdb.com/api.php?amount=${amount_of_questions}&difficulty=${question_difficulty}&category=${question_category}`
+      );
+      const data = await response.data;
+      setQuestions(data.results);
+      return data.results;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <>
+      <Subject />
+      <Question />
+      <AnswerForm />
+      <QuestionNumber />
+    </>
+  );
 };
 
 export default Quiz;
