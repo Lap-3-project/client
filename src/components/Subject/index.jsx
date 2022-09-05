d6301f5101c3c1e0e06150c6819c026f75ce9039
madCakes <jason.pbrooks@hotmail.com>
2022-09-05 12:35:04 +0100

    update/add components

---
+++ src/components/Subject/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Subject = () => {
+  return (
+    //   get subject from state
+    <h2>subject</h2>
+  );
+};
+
+export default Subject;
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,2 +1,3 @@
-export { default as Questions } from "./Questions";
 export { default as Form } from "./Form";
+export { default as Question } from "./Question";
+export { default as Subject } from "./Subject";
