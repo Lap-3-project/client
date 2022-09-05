5d78161a9f29e5d3d1d59059e74b1196d024519e
madCakes <jason.pbrooks@hotmail.com>
2022-09-05 15:13:06 +0100

    add state management(context)

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 
 import { Home, NotFound, Quiz } from "./pages";
+import { DataProvider } from "./context/dataContext";
 import "./App.css";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/Quiz" element={<Quiz />}></Route>
-      <Route path="*" element={<NotFound />}></Route>
-    </Routes>
+    <DataProvider>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/Quiz" element={<Quiz />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </DataProvider>
   );
 }
 
