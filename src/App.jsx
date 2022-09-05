87a7e514c7b4d281273ff289b87e75052f8c5d85
madCakes <jason.pbrooks@hotmail.com>
2022-09-05 12:48:41 +0100

    add Quiz route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 
-import { Home, NotFound } from "./pages";
+import { Home, NotFound, Quiz } from "./pages";
 import "./App.css";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
+      <Route path="/Quiz" element={<Quiz />}></Route>
       <Route path="*" element={<NotFound />}></Route>
     </Routes>
   );
