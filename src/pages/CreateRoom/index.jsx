8d10a718d61a9bb07f119034872ae23515152246
madCakes <jason.pbrooks@hotmail.com>
2022-09-08 15:27:14 +0100

    add socket

--- src/pages/CreateRoom/index.jsx
+++ src/pages/CreateRoom/index.jsx
@@ -1,10 +1,44 @@
 import React, { useContext } from "react";
 import { MultiPlayerLogin, SinglePlayerLogin } from "../../components";
+//💥💥💥
+import io from "socket.io-client";
 
 import DataContext from "../../context/dataContext";
 
 const CreateRoom = () => {
   const { playerMode } = useContext(DataContext);
+  // Socket 💥
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [lastPong, setLastPong] = useState(null);
+
+  // "local host for now until deployment"
+  const url = "http://localhost:3001";
+  const socket = io(url);
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      setIsConnected(true);
+    });
+
+    socket.on("disconnect", () => {
+      setIsConnected(false);
+    });
+
+    socket.on("pong", () => {
+      setLastPong(new Date().toISOString());
+    });
+
+    // Cleanup*
+    return () => {
+      socket.off("connect");
+      socket.off("disconnect");
+      socket.off("pong");
+    };
+  }, []);
+
+  const sendPing = () => {
+    socket.emit("ping");
+  };
 
   return (
     <>
